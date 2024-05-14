7080a77669d7e6c23e68b1b000e5d1eb3dfb4744
Viihgx <vitoriasamara.g77@gmail.com>
2024-05-14 16:44:12 -0300

    :lipstick: Adiciona Css a pagina

--- src/MapFrota.tsx
+++ src/MapFrota.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { obterRotas, adicionarRota } from './db/db';
+import './MapFrota.css';
 
 const MapFrota = () => {
     const [rotas, setRotas] = useState<Array<{bairro: string, horario_coleta: string, dia_coleta: string}>>([]);
@@ -25,20 +26,7 @@ const MapFrota = () => {
     };
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>Bairro:</label>
-                <input type="text" value={bairro} onChange={(e) => setBairro(e.target.value)} />
-
-                <label>Horário de Coleta:</label>
-                <input type="text" value={horarioColeta} onChange={(e) => setHorarioColeta(e.target.value)} />
-
-                <label>Dia de Coleta:</label>
-                <input type="text" value={diaColeta} onChange={(e) => setDiaColeta(e.target.value)} />
-
-                <button type="submit">Adicionar Rota</button>
-            </form>
-
+        <div className='container-master'>
             <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -50,14 +38,44 @@ const MapFrota = () => {
                     </Popup>
                 </Marker>
             </MapContainer>
+            <div className='container-main-results'>
+                <div className="form-container">
+                    <h2>Adicione uma rota ao banco de dados:</h2>
+                    <form onSubmit={handleSubmit}>
+                        <label>Bairro:</label>
+                        <input type="text" value={bairro} onChange={(e) => setBairro(e.target.value)} />
 
-            <div>
-                <h2>Rotas existentes:</h2>
-                <ul>
-                    {rotas.map((rota, index) => (
-                        <li key={index}>{rota.bairro} - {rota.horario_coleta} - {rota.dia_coleta}</li>
-                    ))}
-                </ul>
+                        <label>Horário de Coleta:</label>
+                        <input type="text" value={horarioColeta} onChange={(e) => setHorarioColeta(e.target.value)} />
+
+                        <label>Dia de Coleta:</label>
+                        <input type="text" value={diaColeta} onChange={(e) => setDiaColeta(e.target.value)} />
+
+                        <button type="submit">Adicionar Rota</button>
+                    </form>
+                </div>
+
+                <div className="routes-container">
+                    <h2>Rotas existentes:</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Bairro</th>
+                                <th>Horário de Coleta</th>
+                                <th>Dia de Coleta</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {rotas.map((rota, index) => (
+                                <tr key={index}>
+                                <td>{rota.bairro}</td>
+                                <td>{rota.horario_coleta}</td>
+                                <td>{rota.dia_coleta}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
